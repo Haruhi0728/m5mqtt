95f3d1966cf0ddefc2398de5e685ef7419e43a6a
wawaa128 <akiki0128berurin@gmail.com>
2026-07-09 14:09:11 +0900

    状態検知とか

--- src/main.cpp
+++ src/main.cpp
@@ -13,7 +13,7 @@ const char* mqtt_server = "broker.hivemq.com";
 const int mqtt_port = 1883;
 
 const char* topic_sub = "m5stack/device_02/servo";
-
+const char* topic_pub = "m5stack/device_02/status";
 // ===== MQTT =====
 WiFiClient espClient;
 PubSubClient client(espClient);
@@ -21,6 +21,12 @@ PubSubClient client(espClient);
 // ===== Servo =====
 Servo myServo;
 int servoPin = 21;
+
+// ===== State Detect =====
+const int SW_UNLOCK = 22;
+const int SW_LOCK   = 19;
+
+int lastState = -1;
 
 // ===== MQTT受信 =====
 void callback(char* topic, byte* payload, unsigned int length) {
@@ -34,7 +40,6 @@ void callback(char* topic, byte* payload, unsigned int length) {
     Serial.println("受信:");
     Serial.println(json);
 
-    // JSON解析
     DynamicJsonDocument doc(256);
 
     DeserializationError error = deserializeJson(doc, json);
@@ -53,12 +58,11 @@ void callback(char* topic, byte* payload, unsigned int length) {
     Serial.print("duration: ");
     Serial.println(duration);
 
-    // ===== サーボ制御 =====
+    // サーボ制御
     myServo.write(angle);
 
     delay(duration);
 
-    // 停止
     myServo.write(90);
 
     M5.Lcd.fillScreen(BLACK);
@@ -95,6 +99,73 @@ void reconnect() {
     }
 }
 
+// ===== 鍵状態判定 =====
+void checkLockState() {
+
+    bool unlockSW = (digitalRead(SW_UNLOCK) == LOW);
+    bool lockSW   = (digitalRead(SW_LOCK) == LOW);
+
+    int state;
+
+    if (unlockSW && !lockSW) {
+        state = 0; // 開錠
+    }
+    else if (!unlockSW && lockSW) {
+        state = 1; // 施錠
+    }
+    else if (!unlockSW && !lockSW) {
+        state = 2; // 中間
+    }
+    else {
+        state = 3; // 異常
+    }
+
+    if (state != lastState) {
+
+        String sendMsg = "";
+
+        switch (state) {
+            
+            case 0:
+                Serial.println("UNLOCK");
+                M5.Lcd.fillScreen(BLACK);
+                M5.Lcd.setCursor(20, 20);
+                M5.Lcd.println("UNLOCK");
+                sendMsg = "2,1";
+                break;
+
+            case 1:
+                Serial.println("LOCK");
+                M5.Lcd.fillScreen(BLACK);
+                M5.Lcd.setCursor(20, 20);
+                M5.Lcd.println("LOCK");
+                sendMsg = "2,2";
+                break;
+
+            case 2:
+                Serial.println("MIDDLE");
+                M5.Lcd.fillScreen(BLACK);
+                M5.Lcd.setCursor(20, 20);
+                M5.Lcd.println("MIDDLE");
+                sendMsg = "2,3";
+                break;
+
+            case 3:
+                Serial.println("ERROR");
+                M5.Lcd.fillScreen(BLACK);
+                M5.Lcd.setCursor(20, 20);
+                M5.Lcd.println("ERROR");
+                sendMsg = "2,4";
+                break;
+        }
+        client.publish(topic_pub, sendMsg.c_str());
+        Serial.print("MQTT送信: ");
+        Serial.println(sendMsg);
+
+        lastState = state;
+    }
+}
+
 // ===== setup =====
 void setup() {
 
@@ -106,9 +177,11 @@ void setup() {
 
     // Servo
     myServo.attach(servoPin);
-
-    // 停止位置
     myServo.write(90);
+
+    // Switch
+    pinMode(SW_UNLOCK, INPUT_PULLUP);
+    pinMode(SW_LOCK, INPUT_PULLUP);
 
     // WiFi
     WiFi.begin(ssid, password);
@@ -125,7 +198,6 @@ void setup() {
 
     // MQTT
     client.setServer(mqtt_server, mqtt_port);
-
     client.setCallback(callback);
 }
 
@@ -137,4 +209,9 @@ void loop() {
     }
 
     client.loop();
+
+    // 状態監視
+    checkLockState();
+
+    delay(20);
 }
